1b574f33bd553b5faa69f2dc76acb9519c528dc4
unknown <redoyk2015@gmail.com>
2023-04-09 21:29:42 +0600

    finalized projects

--- index.js
+++ index.js
@@ -229,6 +229,15 @@ async function run() {
             res.send(result)
         })
 
+        //cancel orders
+        app.delete('/cancelOrders/:id', jwtVerify, verifyCustomer, async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.send(result)
+        })
+
         //create admin
         app.put('/admin/:id', jwtVerify, verifyAdmin, async (req, res) => {
 
